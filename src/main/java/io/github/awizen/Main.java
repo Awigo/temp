70b1616c84f8ac5d1f2abf29eb34c365fd77ffec
awizen <awizen.adam@gmail.com>
2022-05-09 20:32:49 +0200

    add gitignore

---
+++ src/main/java/io/github/awizen/Main.java
@@ -0,0 +1,15 @@
+package io.github.awizen;
+
+import java.io.Serializable;
+import java.math.BigInteger;
+import java.util.stream.Stream;
+
+class Main {
+    public static void main(String[] args) {
+        Stream<String> stringStream = Stream.of("ala", "ma", "kota");
+        var build = Stream.<String>builder().build();
+        var generate = Stream.generate(() -> "Hello");
+        boolean b = generate.allMatch(w -> w.length() == 5);
+        System.out.println(b);
+    }
+}
